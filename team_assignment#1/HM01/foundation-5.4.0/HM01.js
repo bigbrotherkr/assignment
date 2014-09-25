cde8640420e5fd15d39ffaa31fc84226b548adbd
Seungjae Jung <jsj940117@gmail.com>
2014-09-25 22:51:03 +0900

    과제중간 js코드 수정

--- team_assignment#1/HM01/foundation-5.4.0/HM01.js
+++ team_assignment#1/HM01/foundation-5.4.0/HM01.js
@@ -4,14 +4,12 @@ window.onload = function(){
   window.onscroll = navbarPosition;
 }
 function navbarPosition(){
-  alert('merong');
   var bar = document.getElementById("navbar");
   if(window.pageYOffset >= 70) {
-  	alert('merong');
   	bar.style.position = "fixed";
-  	bar.style.top = 0px;
+  	bar.style.top = "0px";
   }
-  //else{
-  	//bar.style.position = "relative";
-  //}
+  else{
+  	bar.style.position = "relative";
+  }
 }
